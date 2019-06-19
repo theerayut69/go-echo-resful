8e2393e5468eb139cc5e7b4d8dd026d1b77c1dd2
Theerayut Thanyaphoo <theerayut.t@mono.co.th>
2019-06-19 16:20:37 +0700

    create docker-compose.yml db

--- server.go
+++ server.go
@@ -2,16 +2,43 @@ package main
 
 import (
 	"log"
+	"net/http"
 
+	_ "github.com/go-sql-driver/mysql"
+
+	"github.com/jinzhu/gorm"
 	"github.com/labstack/echo"
 )
+
+type Customer struct {
+	Id        uint   `gorm:"primary_key" json:"id"`
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName"`
+	Age       int    `json:"age"`
+	Email     string `json:"email"`
+}
 
 type CustomerHandler struct {
 	DB *gorm.DB
 }
 
+func main() {
+	e := echo.New()
+
+	h := CustomerHandler{}
+	h.Initialize()
+
+	e.GET("/customers", h.GetAllCustomer)
+	e.POST("/customers", h.SaveCustomer)
+	e.GET("/customers/:id", h.GetCustomer)
+	e.PUT("/customers/:id", h.UpdateCustomer)
+	e.DELETE("/customers/:id", h.DeleteCustomer)
+
+	e.Logger.Fatal(e.Start(":8080"))
+}
+
 func (h *CustomerHandler) Initialize() {
-	db, err := gorm.Open("mysql", "webservice:P@ssw0rd@tcp(127.0.0.1:3306)/db_webservice?charset=utf8&parseTime=true")
+	db, err := gorm.Open("mysql", "root:root@tcp(127.0.0.1:8000)/db_webservice?charset=utf8&parseTime=True")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -21,16 +48,69 @@ func (h *CustomerHandler) Initialize() {
 	h.DB = db
 }
 
-func main() {
-	e := echo.New()
+func (h *CustomerHandler) GetAllCustomer(c echo.Context) error {
+	customers := []Customer{}
 
-	h := CustomerHandler{}
-	h.Initialize()
-	e.GET("/customers", h.GetAllCustomer)
-	e.POST("/customers", h.SaveCustomer)
-	e.GET("/customers/:id", h.GetCustomer)
-	e.PUT("/customers/:id", h.UpdateCustomer)
-	e.DELETE("/customers/:id", h.DeleteCustomer)
+	h.DB.Find(&customers)
 
-	e.Logger.Fatal(e.Start(":8080"))
+	return c.JSON(http.StatusOK, customers)
+}
+
+func (h *CustomerHandler) GetCustomer(c echo.Context) error {
+	id := c.Param("id")
+	customer := Customer{}
+
+	if err := h.DB.Find(&customer, id).Error; err != nil {
+		return c.NoContent(http.StatusNotFound)
+	}
+
+	return c.JSON(http.StatusOK, customer)
+}
+
+func (h *CustomerHandler) SaveCustomer(c echo.Context) error {
+	customer := Customer{}
+
+	if err := c.Bind(&customer); err != nil {
+		return c.NoContent(http.StatusBadRequest)
+	}
+
+	if err := h.DB.Save(&customer).Error; err != nil {
+		return c.NoContent(http.StatusInternalServerError)
+	}
+
+	return c.JSON(http.StatusOK, customer)
+}
+
+func (h *CustomerHandler) UpdateCustomer(c echo.Context) error {
+	id := c.Param("id")
+	customer := Customer{}
+
+	if err := h.DB.Find(&customer, id).Error; err != nil {
+		return c.NoContent(http.StatusNotFound)
+	}
+
+	if err := c.Bind(&customer); err != nil {
+		return c.NoContent(http.StatusBadRequest)
+	}
+
+	if err := h.DB.Save(&customer).Error; err != nil {
+		return c.NoContent(http.StatusInternalServerError)
+	}
+
+	return c.JSON(http.StatusOK, customer)
+}
+
+func (h *CustomerHandler) DeleteCustomer(c echo.Context) error {
+	id := c.Param("id")
+	customer := Customer{}
+
+	if err := h.DB.Find(&customer, id).Error; err != nil {
+		return c.NoContent(http.StatusNotFound)
+	}
+
+	if err := h.DB.Delete(&customer).Error; err != nil {
+		return c.NoContent(http.StatusInternalServerError)
+	}
+
+	return c.NoContent(http.StatusNoContent)
 }
